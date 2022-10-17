ce3fcf5f1417dbfa858887d08a1a0bf99107dbdd
Vinay Thak <86978140+vinu09062002@users.noreply.github.com>
2022-10-17 14:44:12 +0530

    Add files via upload

---
+++ Voice-Based-Email-Mobile-Application-for-Visually-Impaired-main/app/src/main/java/hmi/voicemail/Config.java
@@ -0,0 +1,6 @@
+package hmi.voicemail;
+
+public class Config {
+    public static final String EMAIL ="voiceemail7@gmail.com";  //gmail address
+    public static final String PASSWORD ="password123456#"; //password
+}
